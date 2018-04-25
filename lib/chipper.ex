8258613d3945bb2cbdee6813ffa0abca78af3628
Craig S. Cottingham <craig@cottingham.net>
2018-04-25 14:32:39 -0500

    Initial import

---
+++ lib/chipper.ex
@@ -0,0 +1,18 @@
+defmodule Chipper do
+  @moduledoc """
+  Documentation for Chipper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chipper.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
