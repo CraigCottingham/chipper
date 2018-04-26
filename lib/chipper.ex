d1e5ccf1b31831af285e0b5f23cdebafebf1364a
Craig S. Cottingham <craig@cottingham.net>
2018-04-26 05:34:38 -0500

    Add main/1 to Chipper

--- lib/chipper.ex
+++ lib/chipper.ex
@@ -4,15 +4,22 @@ defmodule Chipper do
   """
 
   @doc """
-  Hello world.
+  Entry point for CLI tool.
 
   ## Examples
 
-      iex> Chipper.hello
-      :world
+      iex> Chipper.main
+      "Not implemented yet"
 
   """
-  def hello do
-    :world
+  @spec main(list()) :: none()
+  def main(args \\ []) do
+    {_opts, _, _} = OptionParser.parse(
+                      args,
+                      switches: [start: :integer, end: :integer, src: :string, dest: :string, update_tags: :string],
+                      aliases: [s: :start, e: :end, S: :src, d: :dest, u: :update_tags]
+                    )
+    IO.puts "Not implemented yet"
   end
+
 end
