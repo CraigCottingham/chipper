f8c4edf05c12a3b36dc7d42614028440c09a08e6
Craig S. Cottingham <craig@cottingham.net>
2018-04-28 15:46:46 -0500

    Add doctests for specific section types

--- test/chipper_test.exs
+++ test/chipper_test.exs
@@ -1,7 +1,27 @@
 defmodule ChipperTest do
   use ExUnit.Case
   doctest Chipper
-  doctest Chipper.IFF
   doctest Chipper.BEAM
   doctest Chipper.BinaryUtils
+  doctest Chipper.IFF
+  doctest Chipper.Section
+  doctest Chipper.Section.Abst
+  doctest Chipper.Section.Atom
+  doctest Chipper.Section.Attr
+  doctest Chipper.Section.AtU8
+  doctest Chipper.Section.CatT
+  doctest Chipper.Section.CInf
+  doctest Chipper.Section.Code
+  doctest Chipper.Section.Dbgi
+  doctest Chipper.Section.ExDc
+  doctest Chipper.Section.ExDp
+  doctest Chipper.Section.ExpT
+  doctest Chipper.Section.FunT
+  doctest Chipper.Section.ImpT
+  doctest Chipper.Section.Line
+  doctest Chipper.Section.LitT
+  doctest Chipper.Section.LocT
+  doctest Chipper.Section.StrT
+
+
 end
