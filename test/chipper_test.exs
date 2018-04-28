e678e261a85602ecb32300065125e6263adbb6e6
Craig S. Cottingham <craig@cottingham.net>
2018-04-28 15:59:04 -0500

    Start filling in a unit test
    Mainly for inspection purposes.

--- test/chipper_test.exs
+++ test/chipper_test.exs
@@ -1,5 +1,8 @@
 defmodule ChipperTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  require Logger
+
   doctest Chipper
   doctest Chipper.BEAM
   doctest Chipper.BinaryUtils
@@ -23,5 +26,43 @@ defmodule ChipperTest do
   doctest Chipper.Section.LocT
   doctest Chipper.Section.StrT
 
+  test "disassemble Elixir.Hello.beam" do
+    {:ok, beam_size, sections} = Chipper.main(["test/Elixir.Hello.beam"])
+    assert beam_size == 900
+    assert is_list(sections)
+    assert Enum.count(sections) == 12
 
+    for section <- sections do
+      case section do
+        {"AtU8", 119} ->
+          assert true
+        {"Code", 143} ->
+          assert true
+        {"StrT", 0} ->
+          assert true
+        {"ImpT", 28} ->
+          assert true
+        {"ExpT", 40} ->
+          assert true
+        {"LocT", 4} ->
+          assert true
+        {"Attr", 40} ->
+          assert true
+        {"CInf", 105} ->
+          assert true
+        {"Dbgi", 169} ->
+          assert true
+        {"ExDc", 86} ->
+          assert true
+        {"ExDp", 27} ->
+          assert true
+        {"Line", 20} ->
+          assert true
+        {chunk_name, chunk_length} ->
+          Logger.error(fn -> "unknown chunk name #{inspect chunk_name} with length #{chunk_length}" end)
+        other ->
+          Logger.error(fn -> "unrecognized chunk #{inspect other}" end)
+      end
+    end
+  end
 end
