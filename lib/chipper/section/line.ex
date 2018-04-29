4f198090df3e645f74a1e6f997ff9a44fc952a74
Craig S. Cottingham <craig@cottingham.net>
2018-04-29 00:00:48 -0500

    Start implementing Line section

--- lib/chipper/section/line.ex
+++ lib/chipper/section/line.ex
@@ -8,6 +8,8 @@ defmodule Chipper.Section.Line do
 
     section -> "Line" chunk_size chunk_data chunk_padding.
 
+    chunk_data -> version flags line_instr_count num_line_refs num_filenames.
+
   """
 
   require Logger
@@ -21,9 +23,30 @@ defmodule Chipper.Section.Line do
   def read(stream) do
     {:ok, <<chunk_length::big-unsigned-integer-size(32)>>, stream} = Chipper.BinaryUtils.read_4(stream)
 
-    {:ok, chunk_data, stream} = Chipper.BinaryUtils.read_n(stream, chunk_length)
+    {:ok, version, stream} = Chipper.BinaryUtils.read_u32_big(stream)
+    {:ok, flags, stream} = Chipper.BinaryUtils.read_4(stream)
+    {:ok, line_instr_count, stream} = Chipper.BinaryUtils.read_u32_big(stream)
+    {:ok, num_line_refs, stream} = Chipper.BinaryUtils.read_u32_big(stream)
+    {:ok, num_filenames, stream} = Chipper.BinaryUtils.read_u32_big(stream)
+    {:ok, chunk_data, stream} = Chipper.BinaryUtils.read_n(stream, (chunk_length - 20))
+
     stream = Stream.drop(stream, (Chipper.BinaryUtils.pad_to_multiple(chunk_length, 4) - chunk_length))
 
-    {:ok, {"Line", chunk_length, chunk_data}, stream}
+    {
+      :ok,
+      {
+        "Line",
+        chunk_length,
+        %{
+          version: version,
+          flags: flags,
+          line_instr_count: line_instr_count,
+          num_line_refs: num_line_refs,
+          num_filenames: num_filenames,
+          remaining_data: chunk_data
+        }
+      },
+      stream
+    }
   end
 end
