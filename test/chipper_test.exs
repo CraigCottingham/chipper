de16794b53aa957248cd0239b4d0c3f9abba64a5
Craig S. Cottingham <craig@cottingham.net>
2018-04-29 00:01:03 -0500

    More output in chipper_test.exs

--- test/chipper_test.exs
+++ test/chipper_test.exs
@@ -34,33 +34,62 @@ defmodule ChipperTest do
 
     for section <- sections do
       case section do
+        {"Attr", 40, data} ->
+          # Logger.debug(fn -> "Attr: #{inspect data}" end)
+          assert is_list(data.term)
+          assert (data.term |> List.first |> elem(0)) == :vsn
+
         {"AtU8", 119, data} ->
-          Logger.debug(fn -> inspect data end)
+          Logger.debug(fn -> "AtU8: #{inspect data}" end)
           assert true
-        {"Code", 143, _} ->
+
+        {"CInf", 105, data} ->
+          # Logger.debug(fn -> "CInf: #{inspect data}" end)
           assert true
-        {"StrT", 0, _} ->
+
+        {"Code", 143, data} ->
+          Logger.debug(fn -> "Code: #{inspect data}" end)
           assert true
-        {"ImpT", 28, _} ->
+
+        {"Dbgi", 169, data} ->
+          # Logger.debug(fn -> "Dbgi: #{inspect data}" end)
           assert true
-        {"ExpT", 40, _} ->
+
+        {"ExDc", 86, data} ->
+          # Logger.debug(fn -> "ExDc: #{inspect data}" end)
           assert true
-        {"LocT", 4, _} ->
+
+        {"ExDp", 27, data} ->
+          # Logger.debug(fn -> "ExDp: #{inspect data}" end)
           assert true
-        {"Attr", 40, _} ->
+
+        {"ExpT", 40, data} ->
+          Logger.debug(fn -> "ExpT: #{inspect data}" end)
           assert true
-        {"CInf", 105, _} ->
+
+        {"ImpT", 28, data} ->
+          Logger.debug(fn -> "ImpT: #{inspect data}" end)
           assert true
-        {"Dbgi", 169, _} ->
+
+        {"Line", 20, data} ->
+          # Logger.debug(fn -> "Line: #{inspect data}" end)
+          assert data.version == 0
+          # assert data.flags
+          assert data.line_instr_count == 8
+          assert data.num_line_refs == 0
+          assert data.num_filenames == 0
+
+        {"LocT", 4, data} ->
+          Logger.debug(fn -> "LocT: #{inspect data}" end)
           assert true
-        {"ExDc", 86, _} ->
+
+        {"StrT", 0, data} ->
+          Logger.debug(fn -> "StrT: #{inspect data}" end)
           assert true
-        {"ExDp", 27, _} ->
-          assert true
-        {"Line", 20, _} ->
-          assert true
+
         {chunk_name, chunk_length} ->
           Logger.error(fn -> "unknown chunk name #{inspect chunk_name} with length #{chunk_length}" end)
+
         other ->
           Logger.error(fn -> "unrecognized chunk #{inspect other}" end)
       end
