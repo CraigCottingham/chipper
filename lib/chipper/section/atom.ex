dfee390d1ff6e22d97a7d1e94810d75a48d6d1a8
Craig S. Cottingham <craig@cottingham.net>
2018-04-29 22:46:48 -0500

    Implement Atom and AtU8

--- lib/chipper/section/atom.ex
+++ lib/chipper/section/atom.ex
@@ -19,11 +19,32 @@ defmodule Chipper.Section.Atom do
   """
   @spec read(any()) :: any()
   def read(stream) do
-    {:ok, <<chunk_length::big-unsigned-integer-size(32)>>, stream} = Chipper.BinaryUtils.read_4(stream)
+    {:ok, chunk_length, stream} = Chipper.BinaryUtils.read_u32_big(stream)
 
-    {:ok, chunk_data, stream} = Chipper.BinaryUtils.read_n(stream, chunk_length)
+    {:ok, num_atoms, stream} = Chipper.BinaryUtils.read_u32_big(stream)
+
+    {atoms, stream} = Enum.reduce(
+                        1..num_atoms,
+                        {[], stream},
+                        fn _, {atoms, stream} ->
+                          {:ok, atom_length, stream} = Chipper.BinaryUtils.read_u8(stream)
+                          {:ok, atom, stream} = Chipper.BinaryUtils.read_n(stream, atom_length)
+                          {[atoms, atom], stream}
+                        end
+                      )
+
     stream = Stream.drop(stream, (Chipper.BinaryUtils.pad_to_multiple(chunk_length, 4) - chunk_length))
 
-    {:ok, {"Atom", chunk_length, chunk_data}, stream}
+    {
+      :ok,
+      {
+        "Atom",
+        chunk_length,
+        %{
+          atoms: List.flatten(atoms)
+        }
+      },
+      stream
+    }
   end
 end
